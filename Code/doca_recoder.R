b7d66ec824d9697570edb942d0b1d796fb282537
Neilblund <neilblund@gmail.com>
2025-09-16 12:53:01 -0400

    event data collection instructions

--- Code/doca_recoder.R
+++ Code/doca_recoder.R
@@ -6,7 +6,7 @@ docaRecoder<-function(doca){
   }
   
   
-  speclist<-jsonlite::fromJSON("https://raw.githubusercontent.com/Neilblund/APAN/refs/heads/main/codebook.json")
+  speclist<-jsonlite::fromJSON('https://raw.githubusercontent.com/Neilblund/GVPT429KFall2025/refs/heads/main/Code/codebook.json')
   
   labels<-speclist$labels
   # function to apply labels to variables
@@ -56,6 +56,42 @@ docaRecoder<-function(doca){
   
   return(doca)
 }
+
+
+
+doca$form_grouping<-factor(as.numeric(doca$form1), levels=group_levels, labels=group_labels)
+
+
+group_levels<-c(1, 2, 5, 4, 13, 14, 8, 16, 9, 15, 18)
+
+
+group_labels<- c(
+  "Rallies,Demonstrations",
+  "Rallies,Demonstrations",
+  "Withholding obligations",
+  "Withholding obligations",
+  "Withholding obligations",
+  "Withholding obligations",
+  "Conventional",
+  "Conventional",
+  "Conventional",
+  "Conventional",
+  "Conventional"
+)
+
+
+doca_selected<-doca|>
+  drop_na(form_grouping, smonamed)
+
+
+doca_selected|>
+  count(smonamed, form_grouping)|>
+  group_by(smonamed)|>
+  mutate(total = sum(n),
+         proportion =  n/total)
+         
+         
+
 
 
 
